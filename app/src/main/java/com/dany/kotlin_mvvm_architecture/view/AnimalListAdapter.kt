9d1c8c6e4ae34afc794b137ef124bb85bbfd31f3
erikaosgue <1476@holbertonschool.com>
2021-05-14 20:52:11 -0400

    Create the Adapter Class

---
+++ app/src/main/java/com/dany/kotlin_mvvm_architecture/view/AnimalListAdapter.kt
@@ -0,0 +1,35 @@
+package com.dany.kotlin_mvvm_architecture.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dany.kotlin_mvvm_architecture.R
+import com.dany.kotlin_mvvm_architecture.model.Animal
+import kotlinx.android.synthetic.main.item_animal.view.*
+
+class AnimalListAdapter(private val animalList: ArrayList<Animal>):
+	RecyclerView.Adapter<AnimalListAdapter.AnimalViewHolder>(){
+
+	fun updateAnimalList(newAnimalList: List<Animal>) {
+		animalList.clear()
+		animalList.addAll(newAnimalList)
+		notifyDataSetChanged()
+	}
+
+	class AnimalViewHolder(view: View): RecyclerView.ViewHolder(view)
+
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalViewHolder {
+		val inflater = LayoutInflater.from(parent.context)
+		val view = inflater.inflate(R.layout.item_animal, parent, false)
+		return AnimalViewHolder(view)
+	}
+
+	override fun onBindViewHolder(holder: AnimalViewHolder, position: Int) {
+		holder.itemView.animalName.text = animalList[position].name
+	}
+
+	override fun getItemCount(): Int {
+		return animalList.size
+	}
+}
