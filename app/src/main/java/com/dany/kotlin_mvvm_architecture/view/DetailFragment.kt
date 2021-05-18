7abf35d8fb7a0d9ab7ea5902179d1607374d3033
erikaosgue <1476@holbertonschool.com>
2021-05-18 17:13:09 -0400

    Add palette Library to chose color base on an Image

--- app/src/main/java/com/dany/kotlin_mvvm_architecture/view/DetailFragment.kt
+++ app/src/main/java/com/dany/kotlin_mvvm_architecture/view/DetailFragment.kt
@@ -1,11 +1,17 @@
 package com.dany.kotlin_mvvm_architecture.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.dany.kotlin_mvvm_architecture.R
 import com.dany.kotlin_mvvm_architecture.model.Animal
 import com.dany.kotlin_mvvm_architecture.util.getProgressDrawable
@@ -40,5 +46,31 @@ class DetailFragment : Fragment() {
 		animalLocation.text = animal?.location
 		animalLifespan.text  = animal?.lifeSpan
 		animalDiet.text = animal?.diet
+
+		animal?.imageUrl?.let { imageUrl ->
+			setUpBackgroundColor(imageUrl)
+		}
+	}
+
+	private fun setUpBackgroundColor(imageUrl: String) {
+
+		Glide.with(this)
+			.asBitmap()
+			.load(imageUrl)
+			.into(object : CustomTarget<Bitmap>() {
+
+				override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+					// Palette is a library Android that choose a color base on an Image and its dominant colors
+					// More info about Palette: https://developer.android.com/training/material/palette-colors
+					// -> resource is the bitmap from the image we load before
+					Palette.from(resource).generate() { palette ->
+							val intColor = palette?.lightMutedSwatch?.rgb ?: 0
+							animalLayout.setBackgroundColor(intColor)
+						}
+				}
+
+				override fun onLoadCleared(placeholder: Drawable?) {
+				}
+			})
 	}
 }
