85c6ad2f0460c5b36ca618de75e4b4a5056d444b
danyramirezg <danyramirezg@gmail.com>
2021-05-18 12:19:39 -0500

    Navigation configuration -video 12

--- app/src/main/java/com/dany/kotlin_mvvm_architecture/view/MainActivity.kt
+++ app/src/main/java/com/dany/kotlin_mvvm_architecture/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dany.kotlin_mvvm_architecture
+package com.dany.kotlin_mvvm_architecture.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dany.kotlin_mvvm_architecture.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
