4efa7c94e61530c0f7e6972bea605c7a7e1c8412
danyramirezg <danyramirezg@gmail.com>
2021-05-23 14:02:17 -0500

    Retrieve the data v-24

--- app/src/main/java/com/dany/kotlin_mvvm_architecture/model/AnimalApi.kt
+++ app/src/main/java/com/dany/kotlin_mvvm_architecture/model/AnimalApi.kt
@@ -2,6 +2,7 @@ package com.dany.kotlin_mvvm_architecture.model
 
 import io.reactivex.Single
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
 
@@ -12,6 +13,7 @@ interface AnimalApi {
     @GET("getKey")
     fun getApiKey(): Single<ApiKey>
 
+    @FormUrlEncoded // Allow us to use the @Field parameters (used with form encoding)
     @POST("getAnimals")
     // This has to take a key parameter
     fun getAnimals(@Field("key") key: String): Single<List<Animal>>
--- app/src/main/java/com/dany/kotlin_mvvm_architecture/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/dany/kotlin_mvvm_architecture/viewmodel/ListViewModel.kt
@@ -3,7 +3,14 @@ package com.dany.kotlin_mvvm_architecture.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.dany.kotlin_mvvm_architecture.model.Animal
+import com.dany.kotlin_mvvm_architecture.model.AnimalApiService
+import com.dany.kotlin_mvvm_architecture.model.ApiKey
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 // This class is going make the connection between the model and the view
 class ListViewModel(application: Application) : AndroidViewModel(application) {
@@ -16,23 +23,69 @@ class ListViewModel(application: Application) : AndroidViewModel(application) {
     val loadError by lazy { MutableLiveData<Boolean>() }
     val loading by lazy { MutableLiveData<Boolean>() }
 
+    private val disposable = CompositeDisposable()
+    private val apiService = AnimalApiService()
+
     fun refresh() {
-        getAnimals()
+        loading.value = true
+        getKey()
+    }
+
+    // A disposable (previously called Subscription in RxJava 1.0) is a tool that can be used to
+    // control the life cycle of an Observable. If the stream of data that the Observable is
+    // producing is boundless, it means that it will stay active forever.
+    private fun getKey() {
+        disposable.add(
+            apiService.getApiKey()
+                .subscribeOn(Schedulers.newThread()) // The operation is performed at the background
+                .observeOn(AndroidSchedulers.mainThread()) // The result is returned in the foreground(mainThread)
+                .subscribeWith(object : DisposableSingleObserver<ApiKey>() {
+                    override fun onSuccess(key: ApiKey) {
+                        if (key.key.isNullOrEmpty()) {
+                            loadError.value = true
+                            loading.value = false
+                        } else {
+                            getAnimals(key.key)
+                        }
+                    }
+
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                        loading.value = false
+                        loadError.value = true
+                    }
+
+                })
+        )
+
     }
 
     // Function to retrieve the information from the backend:
-    private fun getAnimals() {
-        val a1 = Animal("dog")
-        val a2 = Animal("cat")
-        val a3 = Animal("alligator")
-        val a4 = Animal("bee")
-        val a5 = Animal("elephant")
-        val a6 = Animal("flamingo")
+    private fun getAnimals(key: String) {
+        disposable.add(
+            apiService.getAnimals(key)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<List<Animal>>() {
+                    override fun onSuccess(list: List<Animal>) {
+                        loadError.value = false
+                        animals.value = list
+                        loading.value = false
+                    }
 
-        val animalList = arrayListOf(a1, a2, a3, a4, a5, a6)
+                    override fun onError(e: Throwable) {
+                        e.printStackTrace()
+                        loading.value = false
+                        animals.value = null
+                        loadError.value = true
+                    }
 
-        animals.value = animalList
-        loadError.value = false
-        loading.value = false
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
     }
 }
