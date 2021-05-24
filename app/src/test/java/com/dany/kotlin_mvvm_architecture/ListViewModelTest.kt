0257b53e0d445eaf7a8a52304c097067876f271e
erikaosgue <1476@holbertonschool.com>
2021-05-24 16:05:21 -0400

    Test the animals APi success Path

--- app/src/test/java/com/dany/kotlin_mvvm_architecture/ListViewModelTest.kt
+++ app/src/test/java/com/dany/kotlin_mvvm_architecture/ListViewModelTest.kt
@@ -4,13 +4,16 @@ import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.dany.kotlin_mvvm_architecture.di.AppModule
 import com.dany.kotlin_mvvm_architecture.di.DaggerViewModelComponent
+import com.dany.kotlin_mvvm_architecture.model.Animal
 import com.dany.kotlin_mvvm_architecture.model.AnimalApiService
 import com.dany.kotlin_mvvm_architecture.util.SharedPreferencesHelper
 import com.dany.kotlin_mvvm_architecture.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.mockito.Mock
@@ -27,6 +30,8 @@ class ListViewModelTest {
 	@Mock
 	lateinit var prefs: SharedPreferencesHelper
 
+	private val key = "Test key"
+
 	private val application: Application = Mockito.mock(Application::class.java)
 
 	private var listViewModel = ListViewModel(application, true)
@@ -41,6 +46,23 @@ class ListViewModelTest {
 			.prefsModule(PrefsModuleTest(prefs))
 			.build()
 			.inject(listViewModel)
+	}
+
+	// Creating the test
+	fun getAnimalsSuccess() {
+		Mockito.`when`(prefs.getApiKey()).thenReturn(key)
+		val animal = Animal("cow", null, null, null, null, null, null)
+		val animalList = listOf(animal)
+
+		val testSingle = Single.just(animalList)
+
+		Mockito.`when`(animalApiService.getAnimals(key)).thenReturn(testSingle)
+
+		listViewModel.refresh()
+
+		Assert.assertEquals(1, listViewModel.animals.value?.size)
+		Assert.assertEquals(false, listViewModel.loadError.value)
+		Assert.assertEquals(false, listViewModel.loading.value)
 	}
 
 	@Before
