30dcd6ad63d92421cff1ceba5bc7c46c824e474c
danyramirezg <danyramirezg@gmail.com>
2021-05-24 10:40:46 -0500

    palette library final v-30

--- app/src/main/java/com/dany/kotlin_mvvm_architecture/view/DetailFragment.kt
+++ app/src/main/java/com/dany/kotlin_mvvm_architecture/view/DetailFragment.kt
@@ -1,5 +1,7 @@
 package com.dany.kotlin_mvvm_architecture.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,6 +9,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.dany.kotlin_mvvm_architecture.R
 import com.dany.kotlin_mvvm_architecture.Util.getProgressDrawable
 import com.dany.kotlin_mvvm_architecture.Util.loadImage
@@ -41,6 +47,28 @@ class DetailFragment : Fragment() {
         animalLocation.text = animal?.location
         animalLifespan.text = animal?.lifeSpan
         animalDiet.text = animal?.diet
+
+        animal?.imageUrl?.let {
+            setupBackgroundColor(it)
+        }
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource) // Calling the Palette library
+                        .generate() { palette ->
+                            val intColor = palette?.lightMutedSwatch?.rgb ?: 0 // If is null, then 0
+                            animalLayout.setBackgroundColor(intColor)
+                        }
+                }
+            })
     }
 
 }
