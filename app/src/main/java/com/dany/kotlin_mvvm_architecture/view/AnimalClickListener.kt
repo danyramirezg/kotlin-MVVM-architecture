c109c7972562be0dcdeffd7b7db0f56402b77d62
danyramirezg <danyramirezg@gmail.com>
2021-05-24 15:27:59 -0500

    Binding the list items final v-35

---
+++ app/src/main/java/com/dany/kotlin_mvvm_architecture/view/AnimalClickListener.kt
@@ -0,0 +1,7 @@
+package com.dany.kotlin_mvvm_architecture.view
+
+import android.view.View
+
+interface AnimalClickListener {
+    fun onClick(v: View)
+}
--- app/src/main/java/com/dany/kotlin_mvvm_architecture/view/AnimalListAdapter.kt
+++ app/src/main/java/com/dany/kotlin_mvvm_architecture/view/AnimalListAdapter.kt
@@ -3,20 +3,22 @@ package com.dany.kotlin_mvvm_architecture.view
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.dany.kotlin_mvvm_architecture.R
 import com.dany.kotlin_mvvm_architecture.Util.getProgressDrawable
 import com.dany.kotlin_mvvm_architecture.Util.loadImage
+import com.dany.kotlin_mvvm_architecture.databinding.ItemAnimalBinding
 import com.dany.kotlin_mvvm_architecture.model.Animal
 import kotlinx.android.synthetic.main.item_animal.view.*
 
 class AnimalListAdapter(private val animalList: ArrayList<Animal>) :
-    RecyclerView.Adapter<AnimalListAdapter.AnimalViewHolder>() {
+    RecyclerView.Adapter<AnimalListAdapter.AnimalViewHolder>(), AnimalClickListener {
 
     // Update the animal list I want to display
-    fun updateAnimalList(newAnimalList: List<Animal>){
+    fun updateAnimalList(newAnimalList: List<Animal>) {
         animalList.clear()
         animalList.addAll(newAnimalList)
         notifyDataSetChanged()
@@ -27,22 +29,35 @@ class AnimalListAdapter(private val animalList: ArrayList<Animal>) :
         viewType: Int
     ): AnimalViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_animal, parent, false)
+        val view = DataBindingUtil.inflate<ItemAnimalBinding>(
+            inflater,
+            R.layout.item_animal,
+            parent,
+            false
+        )
         return AnimalViewHolder(view)
     }
 
     override fun getItemCount() = animalList.size
 
     override fun onBindViewHolder(holder: AnimalViewHolder, position: Int) {
-        holder.view.animalName.text = animalList[position].name
-        holder.view.animalImage.loadImage(animalList[position].imageUrl,
-            getProgressDrawable(holder.view.context))
-        holder.view.animalLayout.setOnClickListener{
-            val action: NavDirections = ListFragmentDirections.actionDetail(animalList[position])
-            Navigation.findNavController(holder.view).navigate(action)
-        }
+
+        // Using binding:
+        holder.view.animal = animalList[position]
+        holder.view.listener = this
+
     }
 
-    class AnimalViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
+    override fun onClick(v: View) {
+        for (animal in animalList) {
+            if (v.tag == animal.name) {
+                val action = ListFragmentDirections.actionDetail(animal)
+                Navigation.findNavController(v).navigate(action)
+            }
+        }
+
+    }
+
+    class AnimalViewHolder(var view: ItemAnimalBinding) : RecyclerView.ViewHolder(view.root) {
     }
 }
