f9326916b0625bcc3d7e87e2b02f4d55af1608b8
danyramirezg <danyramirezg@gmail.com>
2021-05-25 18:46:22 -0500

    Test the animals API success path

--- app/src/test/java/com/dany/kotlin_mvvm_architecture/ListViewModelTest.kt
+++ app/src/test/java/com/dany/kotlin_mvvm_architecture/ListViewModelTest.kt
@@ -6,14 +6,18 @@ import com.dany.kotlin_mvvm_architecture.Util.SharedPreferencesHelper
 import com.dany.kotlin_mvvm_architecture.di.AppModule
 import com.dany.kotlin_mvvm_architecture.di.DaggerViewModelComponent
 import com.dany.kotlin_mvvm_architecture.di.PrefsModule
+import com.dany.kotlin_mvvm_architecture.model.Animal
 import com.dany.kotlin_mvvm_architecture.model.AnimalApiService
 import com.dany.kotlin_mvvm_architecture.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
@@ -31,10 +35,12 @@ class ListViewModelTest {
 
     val application = Mockito.mock(Application::class.java)
 
-    var listViewModel =  ListViewModel(application)
+    var listViewModel = ListViewModel(application, true)
+
+    private val key = "Test key"
 
     @Before
-    fun setUp(){
+    fun setup() {
         MockitoAnnotations.initMocks(this)
 
         DaggerViewModelComponent.builder()
@@ -43,6 +49,23 @@ class ListViewModelTest {
             .prefsModule(PrefsModuleTest(prefs))
             .build()
             .inject(listViewModel)
+    }
+
+    @Test
+    fun getAnimalsSuccess() {
+        Mockito.`when`(prefs.getApiKey()).thenReturn(key)
+        val animal = Animal("cow", null, null, null, null, null, null)
+        val animalList: List<Animal> = listOf(animal)
+
+        val testSingle = Single.just(animalList)
+
+        Mockito.`when`(animalService.getAnimals(key)).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(1, listViewModel.animals.value?.size)
+        Assert.assertEquals(false, listViewModel.loadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
     }
 
     // Setting up the test
