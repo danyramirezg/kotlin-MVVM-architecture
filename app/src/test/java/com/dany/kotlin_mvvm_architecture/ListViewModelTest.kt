379b703b32669c87e2f50da2297cb217a83fd5aa
erikaosgue <1476@holbertonschool.com>
2021-05-24 20:34:19 -0400

    Test the animals Api failure path

--- app/src/test/java/com/dany/kotlin_mvvm_architecture/ListViewModelTest.kt
+++ app/src/test/java/com/dany/kotlin_mvvm_architecture/ListViewModelTest.kt
@@ -6,23 +6,26 @@ import com.dany.kotlin_mvvm_architecture.di.AppModule
 import com.dany.kotlin_mvvm_architecture.di.DaggerViewModelComponent
 import com.dany.kotlin_mvvm_architecture.model.Animal
 import com.dany.kotlin_mvvm_architecture.model.AnimalApiService
+import com.dany.kotlin_mvvm_architecture.model.ApiKey
 import com.dany.kotlin_mvvm_architecture.util.SharedPreferencesHelper
 import com.dany.kotlin_mvvm_architecture.viewmodel.ListViewModel
 import io.reactivex.Scheduler
 import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.internal.operators.single.SingleJust
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 
 class ListViewModelTest {
 	@get: Rule
-	val rule = InstantTaskExecutorRule()
+	var rule = InstantTaskExecutorRule()
 
 	@Mock
 	lateinit var animalApiService: AnimalApiService
@@ -32,9 +35,10 @@ class ListViewModelTest {
 
 	private val key = "Test key"
 
-	private val application: Application = Mockito.mock(Application::class.java)
+	val application = Mockito.mock(Application::class.java)
 
-	private var listViewModel = ListViewModel(application, true)
+
+	var listViewModel = ListViewModel(application, true)
 
 	@Before
 	fun setUp(){
@@ -47,7 +51,7 @@ class ListViewModelTest {
 			.build()
 			.inject(listViewModel)
 	}
-
+	@Test
 	// Creating the test
 	fun getAnimalsSuccess() {
 		Mockito.`when`(prefs.getApiKey()).thenReturn(key)
@@ -65,6 +69,25 @@ class ListViewModelTest {
 		Assert.assertEquals(false, listViewModel.loading.value)
 	}
 
+	@Test
+	// if there was an Error with the Api retrieving animals
+	fun getAnimalsFailure() {
+		// Get the key
+		Mockito.`when`(prefs.getApiKey()).thenReturn(key)
+
+		val testSingle = Single.error<List<Animal>>(Throwable())
+		val keySingle = Single.just(ApiKey("OK", key))
+
+		Mockito.`when`(animalApiService.getAnimals(key)).thenReturn(testSingle)
+		Mockito.`when`(animalApiService.getApiKey()).thenReturn(keySingle)
+
+		listViewModel.refresh()
+
+		Assert.assertEquals(null, listViewModel.animals.value)
+		Assert.assertEquals(false, listViewModel.loading.value)
+		Assert.assertEquals(true, listViewModel.loadError.value)
+	}
+
 	@Before
 	fun setupRxSchedulers() {
 		val immediate = object: Scheduler() {
@@ -75,6 +98,5 @@ class ListViewModelTest {
 		}
 		RxJavaPlugins.setInitNewThreadSchedulerHandler { immediate }
 		RxAndroidPlugins.setInitMainThreadSchedulerHandler { immediate}
-
 	}
 }
